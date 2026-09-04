aed960fc51d4cbe17fe522713d31b0036e4ae613
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-04 18:17:22 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,7 +1,7 @@
 skill_completed = "Python Syntax"
 exercises_completed = 13
-\
+
 points_per_exercise = 5
 
 point_total = 100
-point_total += exercises_completed + 
+point_total += exercises_completed + points_per_exercise 
