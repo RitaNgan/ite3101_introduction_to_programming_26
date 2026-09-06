281c2a193874cde238c25d704fa03c6606dd24fd
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-06 04:27:52 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -10,7 +10,7 @@ bool_two = None or "Alpha" == "Bravo"
 bool_three = None and "Alpha" == "Bravo" 
 
 # Make me true!
-bool_four = None 
+bool_four = None or "Alpha" == "Bravo" 
 
 # Make me true!
 bool_five = None
