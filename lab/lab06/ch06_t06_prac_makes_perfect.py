73bc6936de073f505a680da86d10c011fc49cc37
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-06 07:57:30 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -5,7 +5,7 @@ def cube(number: int) -> int:
     return number * number * number
 
 
-def by three(number: int) -> Any:
+def by three(number: int)-> Any:
     if number % 3 == 0:
         return cube(number)
     else:
