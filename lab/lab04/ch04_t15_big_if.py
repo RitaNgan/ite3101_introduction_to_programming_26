d49235cd5a726cffe2fb4cabec788e6fb5b5f1e0
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-06 07:08:20 +0000

    [skip actions] lab/lab05/ch05_t01_ahoy.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -6,7 +6,7 @@ def grade_converter(grade: int) -> str:
         return "B"
     elif grade >= 70:
         return "C"
-    elif :
+    elif grade >= 65:
         return "D"
     else:
         return "F"
