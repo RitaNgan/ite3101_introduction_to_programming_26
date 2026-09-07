5f2c85d9b72d110c53d90132789bc2fabd5875e7
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-07 16:14:54 +0000

    [skip actions] lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -2,7 +2,6 @@ def hotel_cost(nights: int) -> int:
     return 140 * nights
 
 def plane_ride_cost(city: str) -> int:
-    if city == "charlotte":
+    if city == "Charlotte":
         return 183
     elif city == "":
-        
