bc1f29fc53bb42e8e1353ce958409e8b4272f1c5
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-07 16:24:10 +0000

    lab/lab07/ch07_t04_transportation.py

--- lab/lab07/ch07_t04_transportation.py
+++ lab/lab07/ch07_t04_transportation.py
@@ -12,7 +12,11 @@ def plane_ride_cost(city: str) -> int:
     elif city == "Los Angeles":
         return 475
 
+
 def rental_car_cost(days: int) -> int:
     cost = days * 40
     if days >= 7:
         cost -= 50
+    elif days >= 3:
+        cost -= 20
+    return cost
