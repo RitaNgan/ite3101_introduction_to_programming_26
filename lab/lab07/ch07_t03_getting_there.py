4401afb0872eadf377cfcfbafd1c724cd550e2c8
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-07 16:16:31 +0000

    [skip actions] lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -6,6 +6,6 @@ def plane_ride_cost(city: str) -> int:
         return 183
     elif city == "Tampa":
         return 220
-    elif city == "Pittsbu":
+    elif city == "Pittsburgh":
         return 222
 
