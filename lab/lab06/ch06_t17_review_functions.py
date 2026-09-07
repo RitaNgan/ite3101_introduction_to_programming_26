f82ab221672dbabf70ee56b9c5d091b69039af14
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-07 15:53:01 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

---
+++ lab/lab06/ch06_t17_review_functions.py
@@ -0,0 +1,7 @@
+def shut_down(s:str)->str:
+       if s == “yes”:
+             return “Shutting down”
+       if s == “no”:
+             return "shutdown aborted"
+       else:
+              return "Sorry"
