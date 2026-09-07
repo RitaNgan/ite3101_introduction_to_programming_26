c2ec1db130aaaf97eb62f66e5d8b0b0cd86bcb38
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-07 16:28:12 +0000

    lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -22,4 +22,4 @@ def rental_car_cost(days: int) -> int:
     return cost
 
 def trip_cost(city: str, days:int) -> int:
-    return rental_car_cost(days) +  hotel_cost(days - 1) + plane_ride_cost()
+    return rental_car_cost(days) +  hotel_cost(days - 1) + plane_ride_cost(city)
