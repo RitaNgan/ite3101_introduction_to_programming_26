44337d497a554c3eb569421505c08adc5182c735
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-10 08:55:08 +0000

    lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -15,5 +15,4 @@ inventory['pouch'].sort()
 inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
-inventory['pocket'] = ['seashell', 'strange berry', 'lint']
-
+inventory['gold'] += 50
