6dc7ebb2a9a4fed194cc17718d7b8f56bd08663d
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-10 08:47:14 +0000

    [skip actions] lab/lab08/ch08_t12_change_yr_mind.py

--- lab/lab08/ch08_t12_change_yr_mind.py
+++ lab/lab08/ch08_t12_change_yr_mind.py
@@ -8,8 +8,6 @@ zoo_animals = {'Unicorn': 'Cotton Candy House',
 
 # Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
 del zoo_animals['Unicorn']
- zoo_animals
-# Your code here!
 del zoo_animals['Sloth']
 
 
