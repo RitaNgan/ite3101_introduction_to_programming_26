b855ed56243774cbd871b03ee31414fe745cc82f
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-10 08:44:18 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -4,6 +4,8 @@ print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
 menu["Icecream"] =3
+menu["Icecream"] =3
+menu["Icecream"] =3
 
 print("There are " + str(len(menu)) + " items on the menu.")
 print(menu)
