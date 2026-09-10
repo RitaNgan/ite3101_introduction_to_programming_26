41e2d9f690be772ce0816ff2e9bb46557a717ce4
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-10 08:02:32 +0000

    [skip actions] lab/lab08/ch08_t04_late_arri_n_list_len.py

--- lab/lab08/ch08_t04_late_arri_n_list_len.py
+++ lab/lab08/ch08_t04_late_arri_n_list_len.py
@@ -3,7 +3,7 @@ suitcase.append("sunglasses")
 
 # Your code here!
 suitcase.append("bathing suit")
-suitcase.append("bathing suit")
+suitcase.append("T-shirt")
 suitcase.append("bathing suit")
 
 list_length = 1  # Set this to the length of suitcase
