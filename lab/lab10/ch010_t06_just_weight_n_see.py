126dd1fba1b07cdda483e856bbaba6cd55d72867
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-21 06:45:06 +0000

    [skip actions] lab/lab10/ch010_t06_just_weight_n_see.py

--- lab/lab10/ch010_t06_just_weight_n_see.py
+++ lab/lab10/ch010_t06_just_weight_n_see.py
@@ -28,4 +28,4 @@ def average(numbers: List[float]) -> float:
 
 def get_average(student:dict) -> float:
     homework = average(student["homework"])
-    quizzes = average(student["homework"])
+    quizzes = average(student["quizzes"])
