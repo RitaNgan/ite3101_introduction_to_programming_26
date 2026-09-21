35456b505c5980ece309391d3c5cedc6afb4ffab
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-21 05:59:18 +0000

    [skip actions] lab/lab10/ch010_t02_what_the_score.py

--- lab/lab10/ch010_t01_lesson_number_one.py
+++ lab/lab10/ch010_t01_lesson_number_one.py
@@ -1,19 +1,19 @@
 lloyd = {
-    "name":"Lloyd",
+    "name": "Lloyd",
     "homework": [],
     "quizzes": [],
     "tests": []
 }
 
-alice ={
-    "name":"Alice",
+alice = {
+    "name": "Alice",
     "homework": [],
     "quizzes": [],
     "tests": []
 }
 
 tyler = {
-    "name":"Tyler",
+    "name": "Tyler",
     "homework": [],
     "quizzes": [],
     "tests": []
