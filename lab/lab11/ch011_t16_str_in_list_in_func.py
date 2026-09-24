f793405ff77724bd5fdf2744c33240af1e86f3cc
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-24 08:37:19 +0000

    [skip actions] lab/lab11/ch011_t16_str_in_list_in_func.py

--- lab/lab11/ch011_t16_str_in_list_in_func.py
+++ lab/lab11/ch011_t16_str_in_list_in_func.py
@@ -3,7 +3,8 @@ from typing import List
 n = ["Michael", "Lieberman"]
 # Add your function here
 
-def join_strings(words: List[str])-> str:
+
+def join_strings(words: List[str]) -> str:
     return = ""
     for word in words:
         return += words
