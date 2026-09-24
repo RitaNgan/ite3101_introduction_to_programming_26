ef8d71172a9ec04f3cb5cce5e5441b0c86e7debe
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-24 08:53:48 +0000

    [skip actions] lab/lab11/ch011_t17_two_lists_two_argu_in_func.py

--- lab/lab11/ch011_t17_two_lists_two_argu_in_func.py
+++ lab/lab11/ch011_t17_two_lists_two_argu_in_func.py
@@ -1,9 +1,14 @@
+from typing import List
+
 m = [1, 2, 3]
 n = [4, 5, 6]
 
 # Add your code here!
+
+
 def join_lists(x: List[int], y: List[int]) -> List[int]:
     return x = y
+
 
 print(join_lists(m, n))
 # You want this to print [1, 2, 3, 4, 5, 6]
