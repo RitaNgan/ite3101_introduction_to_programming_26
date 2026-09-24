4a1f590bf94d8f8884fd4c3c0432d50c365f8534
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-24 08:08:07 +0000

    [skip actions] lab/lab11/ch011_t13_modi_ele_in_list_in_func.py

--- lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
+++ lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
@@ -6,7 +6,7 @@ for i in range(0, len(n)):
 # Don't forget to return your new list!
 
 
-def double_list(x: List[int]) -> List[int]:
+def double_list(x:List[int]) -> List[int]:
     for i in range(0, len(x)):
         x[i] = x[1] * 2
     return x
