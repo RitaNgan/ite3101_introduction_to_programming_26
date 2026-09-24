c2629966e35e193ce60689589b6f48dfad48755a
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-24 07:29:00 +0000

    lab/lab11/ch011_t07_str_in_func.py

--- lab/lab11/ch011_t07_str_in_func.py
+++ lab/lab11/ch011_t07_str_in_func.py
@@ -1,6 +1,9 @@
 n = "Hello"
 # Your function here!
-def string_function(s:str) -> str:
+
+
+def string_function(s: str) -> str:
     return s + "world"
 
-# print(string_function(n))
+
+print(string_function(n))
