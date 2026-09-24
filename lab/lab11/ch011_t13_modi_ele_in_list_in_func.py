3be0707396cf1d85418c679073eb031876135e4f
RitaNgan <260162722@stu.vtc.edu.hk>
2026-09-24 08:05:49 +0000

    [skip actions] lab/lab11/ch011_t13_modi_ele_in_list_in_func.py

--- lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
+++ lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
@@ -1,9 +1,5 @@
 n = [3, 5, 7]
 
-def print_list(x:List[int]) -> None:
-    for i in range(0, len(x)):
-        print(x[i])
-
 for i in range(0, len(n)):
     n[i] = n[i] * 2
 # Don't forget to return your new list!
